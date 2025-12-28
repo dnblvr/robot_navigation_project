c5aef1b0d5fdd25a65e36f1a89fdff31eba0c84c
dnblvr <gianfajardo.prim@gmail.com>
2025-12-28 06:42:42 -0800

    prepared the barebones of the ICM_20948 firmware

--- FW_ICM_20948/inc/EUSCI_B1_I2C.h
+++ FW_ICM_20948/inc/EUSCI_B1_I2C.h
@@ -5,20 +5,22 @@
  * This file contains the function definitions for the EUSCI_B1_I2C driver.
  * The EUSCI_B1_I2C driver uses busy-wait implementation.
  *
- * @note This function assumes that the necessary pin configurations for I2C communication have been performed
- *       on the corresponding pins. The output from the pins will be observed using an oscilloscope.
+ * @note This function assumes that the necessary pin configurations for I2C
+ *  communication have been performed on the corresponding pins. The output
+ *  from the pins will be observed using an oscilloscope.
  *       - P6.4 (SDA)
  *       - P6.5 (SCL)
  *
- * For more information regarding the Enhanced Universal Serial Communication Interface (eUSCI),
- * refer to the MSP432Pxx Microcontrollers Technical Reference Manual
+ * For more information regarding the Enhanced Universal Serial Communication
+ *  Interface (eUSCI), refer to the MSP432Pxx Microcontrollers Technical
+ *  Reference Manual
  *
  * @author Aaron Nanas
- *
+ *          (modified by Gian Fajardo)
  */
 
-#ifndef INC_EUSCI_B1_I2C_H_
-#define INC_EUSCI_B1_I2C_H_
+#ifndef __INC_EUSCI_B1_I2C_H__
+#define __INC_EUSCI_B1_I2C_H__
 
 #include <stdint.h>
 #include "msp.h"
@@ -55,18 +57,23 @@
  *    15-9       Reserved     0x0        Reserved
  *    8          UCETXINT     0x0        Early UCTXIFG0 flag in slave mode
  *    7-6        UCCLTO       0x0        Disable clock low timeout
- *    5          UCSTPNACK    0x0        Send NACK before STOP condition in master receiver mode
- *    4          UCSWACK      0x0        Address acknowledge of slave is controlled by eUSCI module
- *    3-2        UCASTPx      0x0        No automatic STOP generation in slave mode when UCBCNTIFG is available
+ *    5          UCSTPNACK    0x0        Send NACK before STOP condition in
+ *                                          master receiver mode
+ *    4          UCSWACK      0x0        Address acknowledge of slave is
+ *                                          controlled by eUSCI module
+ *    3-2        UCASTPx      0x0        No automatic STOP generation in slave
+ *                                          mode when UCBCNTIFG is available
  *    1-0        UCGLITx      0x0        Deglitch time of 50 ns
  *
- * For setting the SCL frequency, the clock source used is 12 MHz.
- * To achieve a 400 kHz SCL frequency, the BRW value is set to 30.
+ * For setting the SCL frequency, the clock source used is 12 MHz. To achieve a
+ *  400 kHz SCL frequency, the BRW value is set to 30.
  *
- * For more information regarding the registers used, refer to the EUSCI_B I2C Registers section
- * of the MSP432Pxx Microcontrollers Technical Reference Manual.
+ * For more information regarding the registers used, refer to the EUSCI_B I2C
+ *  Registers section of the MSP432Pxx Microcontrollers Technical Reference
+ *  Manual.
  *
- * @note This function assumes that the necessary pin configurations for I2C communication have been performed:
+ * @note This function assumes that the necessary pin configurations for I2C
+ *  communication have been performed:
  *       - P6.4 (SDA, Serial Data)
  *       - P6.5 (SCL, Serial Clock)
  *
@@ -75,72 +82,89 @@
 void EUSCI_B1_I2C_Init();
 
 /**
- * @brief Sends a byte of data to a specified I2C slave device using EUSCI_B1 module.
+ * @brief Sends a byte of data to a specified I2C slave device using EUSCI_B1
+ *  module.
  *
  * This function sends a byte of data to an I2C slave device with the specified
- * slave address using the EUSCI_B1 I2C module. It follows the I2C communication protocol
- * to initiate the transmission, send the data, and terminate the communication.
+ *  slave address using the EUSCI_B1 I2C module. It follows the I2C protocol
+ *  to initiate the transmission, send the data, and terminate the
+ *  communication.
  *
  * @param slave_address The 7-bit address of the I2C slave device.
  * @param data The data byte to be sent to the slave device.
  *
- * @note Before using this function, ensure that the I2C module (EUSCI_B1) has been properly
- *       initialized using the EUSCI_B1_I2C_Init function.
+ * @note Before using this function, ensure that the I2C module (EUSCI_B1) has
+ *  been properly initialized using the EUSCI_B1_I2C_Init function.
  *
  * @return None
  */
 void EUSCI_B1_I2C_Send_A_Byte(uint8_t slave_address, uint8_t data);
 
 /**
- * @brief Sends multiple bytes of data to a specified I2C slave device using EUSCI_B1 module.
+ * @brief Sends multiple bytes of data to a specified I2C slave device using
+ *  EUSCI_B1 module.
  *
- * This function sends an array of data bytes to an I2C slave device with the specified
- * slave address using the EUSCI_B1 I2C module. It follows the I2C communication protocol
- * to initiate the transmission, send the data, and terminate the communication.
+ * This function sends an array of data bytes to an I2C slave device with the
+ *  specified slave address using the EUSCI_B1 I2C module. It follows the I2C
+ *  communication protocol to initiate the transmission, send the data, and
+ *  terminate the communication.
  *
  * @param slave_address The 7-bit address of the I2C slave device.
- * @param data_buffer   A pointer to an array of data bytes to be sent to the slave device.
+ * @param data_buffer   A pointer to an array of data bytes to be sent to the
+ *      slave device.
  * @param packet_length The number of data bytes to send in the data_buffer.
  *
- * @note Before using this function, ensure that the I2C module (EUSCI_B1) has been properly
- *       initialized using the EUSCI_B1_I2C_Init function.
+ * @note Before using this function, ensure that the I2C module (EUSCI_B1) has
+ *      been properly initialized using the EUSCI_B1_I2C_Init function.
  *
  * @return None
  */
-void EUSCI_B1_I2C_Send_Multiple_Bytes(uint8_t slave_address, uint8_t *data_buffer, uint32_t packet_length);
+void EUSCI_B1_I2C_Send_Multiple_Bytes(
+        uint8_t     slave_address,
+        uint8_t*    data_buffer,
+        uint32_t    packet_length);
 
 /**
- * @brief Receives a single byte of data from a specified I2C slave device using EUSCI_B1 module.
+ * @brief Receives a single byte of data from a specified I2C slave device
+ *  using EUSCI_B1 module.
  *
- * This function receives a single byte of data from an I2C slave device with the specified
- * slave address using the EUSCI_B1 I2C module. It follows the I2C communication protocol
- * to initiate the reception, retrieve the data, and terminate the communication.
+ * This function receives a single byte of data from an I2C slave device with
+ *  the specified slave address using the EUSCI_B1 I2C module. It follows the
+ *  I2C communication protocol to initiate the reception, retrieve the data,
+ *  and terminate the communication.
  *
  * @param slave_address The 7-bit address of the I2C slave device.
  *
- * @note Before using this function, ensure that the I2C module (EUSCI_B1) has been properly
- *       initialized using the EUSCI_B1_I2C_Init function.
+ * @note Before using this function, ensure that the I2C module (EUSCI_B1) has
+ *  been properly initialized using the EUSCI_B1_I2C_Init function.
  *
  * @return The received data byte from the slave device.
  */
 uint8_t EUSCI_B1_I2C_Receive_A_Byte(uint8_t slave_address);
 
 /**
- * @brief Receives multiple bytes of data from a specified I2C slave device using EUSCI_B1 module.
+ * @brief Receives multiple bytes of data from a specified I2C slave device
+ *  using EUSCI_B1 module.
  *
- * This function receives an array of data bytes from an I2C slave device with the specified
- * slave address using the EUSCI_B1 I2C module. It follows the I2C communication protocol
- * to initiate the reception, retrieve the data, and terminate the communication.
+ * This function receives an array of data bytes from an I2C slave device with
+ *  the specified slave address using the EUSCI_B1 I2C module. It follows the
+ *  I2C communication protocol to initiate the reception, retrieve the data,
+ *  and terminate the communication.
  *
  * @param slave_address The 7-bit address of the I2C slave device.
- * @param data_buffer   A pointer to an array where received data bytes will be stored.
- * @param packet_length The number of data bytes to receive and store in data_buffer.
+ * @param data_buffer   A pointer to an array where received data bytes will be
+ *      stored.
+ * @param packet_length The number of data bytes to receive and store in
+ *      data_buffer.
  *
- * @note Before using this function, ensure that the I2C module (EUSCI_B1) has been properly
- *       initialized using the EUSCI_B1_I2C_Init function.
+ * @note Before using this function, ensure that the I2C module (EUSCI_B1) has
+ *      been properly initialized using the EUSCI_B1_I2C_Init function.
  *
  * @return None
  */
-void EUSCI_B1_I2C_Receive_Multiple_Bytes(uint8_t slave_address, uint8_t *data_buffer, uint16_t packet_length);
+void EUSCI_B1_I2C_Receive_Multiple_Bytes(
+        uint8_t     slave_address,
+        uint8_t*    data_buffer,
+        uint16_t    packet_length);
 
-#endif /* INC_EUSCI_B1_I2C_H_ */
+#endif /* __INC_EUSCI_B1_I2C_H__ */

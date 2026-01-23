dca29ba46daee27a0cbecd022880ae6328fdc54a
dnblvr <gianfajardo.prim@gmail.com>
2026-01-23 14:41:36 -0800

    additional setup for Nabin to work on this

--- AN_Odometry_1/src/Dynamic_Window_Approach.c
+++ AN_Odometry_1/src/Dynamic_Window_Approach.c
@@ -3,49 +3,7 @@
  * @brief   Implementation of the Dynamic Window Approach (DWA) for local
  */
 
-#include "data_structures.h"
-#include <float.h>
-#include <math.h>
-
-
-/**
- * @brief  
- */
-typedef struct {
-    float   min_speed,
-            max_speed;
-
-
-    float 	max_omega;
-    float 	max_accel;
-    float 	max_omega_accel;
-
-    float 	v_resolution;
-    float 	omega_resolution;
-    float 	dt;
-    float 	predict_time;
-    float 	robot_radius;
-
-    float 	to_goal_cost_gain;
-    float 	speed_cost_gain;
-    float 	obstacle_cost_gain;
-
-} DWA_Config;
-
-
-/**
- * @brief  
- */
-typedef struct  {
-
-    float x;
-    float y;
-    float yaw;
-    float v;          
-    float omega;
-
-} RobotState;
-
+#include "inc/Dynamic_Window_Approach.h"
 
 /**
  * @brief 
@@ -72,24 +30,15 @@ void Config_DWA(DWA_Config* cfg) {
 
 }
 
-/**
- * @brief Maximum trajectory storage length.
- */
-#define MAX_TRAJECTORY_LEN 10
 
+void Config_Robot_State(RobotState* state) {
 
-/**
- * @brief a string of points representing a curved trajectory
- * 
- * @note This must be generated locally so that multiple trajectories can be stored and discarded as needed
- * 
- */
-typedef struct {
-
-    Point2D points[MAX_TRAJECTORY_LEN];
-    uint32_t num_points;
-
-} Predicted_Path;
+    state->x     = 0;
+    state->y     = 0;
+    state->yaw   = 0;
+    state->v     = 0;
+    state->omega = 0;
+}
 
 
 void DWA_Control(
